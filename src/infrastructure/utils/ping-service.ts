1d52bca70ff15efb6db759c34e48318254415d51
marcos.henrique <wakeupmh@gmail.com>
2025-03-15 10:48:27 -0300

    chore: comments

--- src/infrastructure/utils/ping-service.ts
+++ src/infrastructure/utils/ping-service.ts
@@ -8,10 +8,8 @@ import { ping } from './ping';
 // Set up interval for pinging
 const pingInterval = parseInt(process.env.PING_INTERVAL || '300000', 10); // Default 5 minutes
 
-// Initial ping
 ping();
 
-// Set up interval
 setInterval(ping, pingInterval);
 
 console.log('ping service started'.toLowerCase());
